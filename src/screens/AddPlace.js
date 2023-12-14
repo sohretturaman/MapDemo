16d1f5a5dd694181c96087b88cdfde8f9e9a49dd
şöhrett <sohretturaman@gmail.com>
2023-12-15 01:54:40 +0300

    android emulator's camera has been  initialized  with expo camera

--- src/screens/AddPlace.js
+++ src/screens/AddPlace.js
@@ -3,6 +3,7 @@
 import { StyleSheet, Text, View } from "react-native";
 import React, { useState } from "react";
 import PlaceForm from "../components/PlaceForm";
+import { launchCameraAsync, useCameraPermissions } from "expo-image-picker";
 
 const AddPlace = () => {
   const [title, setTitle] = useState("");
@@ -11,13 +12,22 @@ const AddPlace = () => {
     setTitle(value);
     console.log("title", value);
   }
+  async function handleCameraPress() {
+    const cameraResponse = await launchCameraAsync({
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 0.8,
+    });
+
+    console.log("camera respponse", cameraResponse);
+  }
   return (
     <View>
       <PlaceForm
         title={"write a place name"}
         handleTextInput={handleTextInput}
         value={title}
-        onSubmit={() => console.log("presseed to button ")}
+        onSubmit={handleCameraPress}
         buttonTitle={"add a new place"}
       />
     </View>
